62bcc729f4e88f5807b69e61ca33abc30a4502d0
ホアン ゴック シン <h.sinh@bravesoft.co.jp>
2017-12-19 10:34:13 +0900

    add new main

--- main.rs
+++ main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    fizzbuzz_to(100);
+}
+
+fn is_devided_by(lhs: u32, rhs: u32) -> bool {
+     if rhs == 0 {
+         return false;
+     }
+     lhs % rhs == 0
+     
+}
+
+
+fn fizzbuzz(n: u32) {
+    if is_devided_by(n, 15) {
+        println!("fizzbuzz");
+    } else if is_devided_by(n, 3) {
+        println!("fizz");
+    } else {
+        println!("buzz");
+    }
+}
+
+fn fizzbuzz_to(n: u32) {
+    for n in 1..n + 1 {
+        fizzbuzz(n);
+    }
 }
